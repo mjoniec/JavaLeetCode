618005dc469b9d80669a8b563c3bb72334f6bec1
mjoniec <mart1n.joniec@gmail.com>
2022-08-02 16:32:54 +0200

    optimal pairs - only file

--- LeetCodeLauncher/src/Main.java
+++ LeetCodeLauncher/src/Main.java
@@ -1,5 +1,8 @@
+import other.OptimalPairs;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new OptimalPairs().Run();
     }
 }
---
+++ LeetCodeLauncher/src/other/OptimalPairs.java
@@ -0,0 +1,7 @@
+package other;
+
+public class OptimalPairs {
+    public void Run(){
+        System.out.println("OptimalPairs: ");
+    }
+}
