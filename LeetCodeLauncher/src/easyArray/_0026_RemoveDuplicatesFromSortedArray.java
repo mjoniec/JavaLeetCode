f4fa281cdd3d876a4d7082ff86fdf0bdb6a5e4b0
mjoniec <mart1n.joniec@gmail.com>
2022-08-19 20:05:20 +0200

    _0026_RemoveDuplicatesFromSortedArray wip

--- LeetCodeLauncher/src/easyArray/_0026_RemoveDuplicatesFromSortedArray.java
+++ LeetCodeLauncher/src/easyArray/_0026_RemoveDuplicatesFromSortedArray.java
@@ -6,8 +6,11 @@ public class _0026_RemoveDuplicatesFromSortedArray {
 
         int k = 0;
 
-        for(int i=0; i<nums.length-1;i++){
+        for(int i=0; i<nums.length;i++){
+
             if(nums[i] == -1) return k;
+            if(i==nums.length-1) return++k;
+
             if(nums[i]==nums[i+1] && (i+1!=nums.length-1)){
                 for (int j=i+1; j<nums.length-1;j++){
                     nums[j] = nums[j+1];
@@ -25,6 +28,10 @@ public class _0026_RemoveDuplicatesFromSortedArray {
 
     public void run() {
         System.out.println("_0026_RemoveDuplicatesFromSortedArray: ");
+        System.out.println("Input: [1,1]  Expected: 1   Actual: "
+                + removeDuplicates(new int[]{1,1}));
+        System.out.println("Input: [1,2]  Expected: 2   Actual: "
+                + removeDuplicates(new int[]{1,2}));
         System.out.println("Input: [1,1,2]  Expected: 2   Actual: "
                 + removeDuplicates(new int[]{1,1,2}));
         System.out.println("Input: [0,0,1,1,1,2,2,3,3,4]  Expected: 5   Actual: "
