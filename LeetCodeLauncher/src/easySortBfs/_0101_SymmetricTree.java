274f18e226e7544c44cb84f2c42f7d2822108a12
mjoniec <mart1n.joniec@gmail.com>
2022-09-06 15:36:48 +0200

    _0101_SymmetricTree

--- LeetCodeLauncher/src/easySortBfs/_0101_SymmetricTree.java
+++ LeetCodeLauncher/src/easySortBfs/_0101_SymmetricTree.java
@@ -2,17 +2,57 @@ package easySortBfs;
 
 import Utilities.TreeNode;
 
+import java.util.Stack;
+
 public class _0101_SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
+        if(root==null)  return true;
 
+        var stack = new Stack<TreeNode>();
+        TreeNode left, right;
 
-        return false;
+        if(!isSymmetricAddToStack(root.left, root.right, stack)){
+            return false;
+        }
+
+        while(!stack.empty()){
+            if(stack.size() % 2 != 0) return false;//if odd we can stop
+
+            right = stack.pop();
+            left = stack.pop();
+
+            //mirror check & add
+            if(!isSymmetricAddToStack(left.left, right.right, stack)) return false;
+            if(!isSymmetricAddToStack(left.right, right.left, stack)) return false;
+        }
+
+        return true;
+    }
+
+    private boolean isSymmetricAddToStack(TreeNode left, TreeNode right, Stack<TreeNode> s) {
+        if(left == null && right == null){
+            return true;
+        }
+        if(left != null && right == null){
+            return false;
+        }
+        if(left == null && right != null){
+            return false;
+        }
+        if(left.val != right.val) {
+            return false;
+        }
+
+        s.push(left);
+        s.push(right);
+
+        return true;
     }
 
     public void run() {
         System.out.println("_101_SymmetricTree: ");
         System.out.println("Input: symmetric,  Expected: true   Actual: " + isSymmetric(prepareTestTree1()));
-        System.out.println("Input: non symmetric,  Expected: false   Actual: " + isSymmetric(prepareTestTree1()));
+        System.out.println("Input: non symmetric,  Expected: false   Actual: " + isSymmetric(prepareTestTree2()));
     }
 
     private TreeNode prepareTestTree1(){
