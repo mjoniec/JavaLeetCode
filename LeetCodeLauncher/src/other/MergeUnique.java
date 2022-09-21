ba5e4829b33b80b2b23c66bd20f9818014a84da0
mjoniec <mart1n.joniec@gmail.com>
2022-09-21 14:41:25 +0200

    merge unique refactor

--- LeetCodeLauncher/src/other/MergeUnique.java
+++ LeetCodeLauncher/src/other/MergeUnique.java
@@ -1,35 +1,20 @@
 package other;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.*;
 
 public class MergeUnique {
     public int[] mergeUnique(int[] array1, int[] array2){
-        HashSet<Integer> set = new HashSet<Integer>();
-
-        for (int i : array1){
-            if(!set.contains(i)){
-                set.add(i);
-            }
-        }
-
-        for (int i : array2){
-            if(!set.contains(i)){
-                set.add(i);
-            }
-        }
+        Set<Integer> set = new HashSet<>();
+        for (var num : array1) set.add(num);
+        for (var num : array2) set.add(num);
 
         var list = new ArrayList<>(set);
-
         Collections.sort(list);
 
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public void run() {
-
         var t = mergeUnique(
                 new int[]{3, 1, 2, 2, 3},
                 new int[]{5, 1, 2, 5}
