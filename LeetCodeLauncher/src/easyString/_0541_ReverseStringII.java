ac8c35aa5954b82d22681fc163346dd93263fa0c
mjoniec <mart1n.joniec@gmail.com>
2022-09-24 18:19:38 +0200

    _0541_ReverseStringII

--- LeetCodeLauncher/src/easyString/_0541_ReverseStringII.java
+++ LeetCodeLauncher/src/easyString/_0541_ReverseStringII.java
@@ -5,21 +5,20 @@ public class _0541_ReverseStringII {
         var tab = s.toCharArray();
         int index = 0;
 
-        while(index + 2*k <= s.length()){
-            reverse(tab, index, index+k);
-            index+=2*k;
+        while(index+k-1 < s.length()){
+            reverse(tab, index, index+k-1);
+            index += 2*k;
         }
 
         int leftover = s.length() - index;
-        if(leftover < k) reverse(tab, index, leftover);
-        if(leftover >= k && leftover < 2*k) reverse(tab, index, index+k);
+        if(leftover < k) reverse(tab, index, index + leftover - 1);
+        if(leftover >= k && leftover < 2*k) reverse(tab, index, index + k - 1);
 
         return new String(tab);
     }
 
-    private void reverse(char[] tab, int i, int k){
-        int mid = (k - i) / 2;
-        for(int j = k-1; i+mid<k; i++, j--) {
+    private void reverse(char[] tab, int i, int j){
+        for(; i < j; i++, j--) {
             var c = tab[i];
             tab[i] = tab[j];
             tab[j] = c;
@@ -28,9 +27,15 @@ public class _0541_ReverseStringII {
 
     public void run() {
         System.out.println("_0541_ReverseStringII: ");
-        System.out.println("Expected: bacd   Actual:" + reverseStr("abcd", 2));
-        System.out.println("Expected: bacd feg   Actual:" + reverseStr("abcdefg", 2));
+        System.out.println("Expected: ba cd   Actual:" + reverseStr("abcd", 2));
+        System.out.println("Expected: ba cd fe g   Actual:" + reverseStr("abcdefg", 2));
         System.out.println("Expected: dcba   Actual:" + reverseStr("abcd", 4));
-        System.out.println("Expected: gfed cba   Actual:" + reverseStr("abcdefg", 8));
+        System.out.println("Expected: gfedcba   Actual:" + reverseStr("abcdefg", 8));
+        System.out.println("Expected: fdcqkmxwholhytmhafpesaentdvxginrjlyqzyh ehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjql lgsqddebemjanqcqnfkjmi   Actual:" + reverseStr("hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl", 39));
+//"hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjql imjkfnqcqnajmebeddqsgl"
+//39
+//fdcqkmxwholhytmhafpesaentdvxginrjlyqzyh ehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjql imjkfnqcqnajmebeddqsgl
+//fdcqkmxwholhytmhafpesaentdvxginrjlyqzyh ehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjql lgsqddebemjanqcqnfkjmi
+
     }
 }
