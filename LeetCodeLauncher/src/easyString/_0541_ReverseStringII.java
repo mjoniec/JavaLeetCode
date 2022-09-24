a2268e52c8580a99cde5753d3c0581c25052d876
mjoniec <mart1n.joniec@gmail.com>
2022-09-24 17:53:07 +0200

    _0541_ReverseStringII - unfinished

--- LeetCodeLauncher/src/easyString/_0541_ReverseStringII.java
+++ LeetCodeLauncher/src/easyString/_0541_ReverseStringII.java
@@ -5,10 +5,14 @@ public class _0541_ReverseStringII {
         var tab = s.toCharArray();
         int index = 0;
 
-        while(index + k <= s.length()){
+        while(index + 2*k <= s.length()){
             reverse(tab, index, index+k);
             index+=2*k;
         }
+
+        int leftover = s.length() - index;
+        if(leftover < k) reverse(tab, index, leftover);
+        if(leftover >= k && leftover < 2*k) reverse(tab, index, index+k);
 
         return new String(tab);
     }
@@ -24,9 +28,9 @@ public class _0541_ReverseStringII {
 
     public void run() {
         System.out.println("_0541_ReverseStringII: ");
-        System.out.println("Expected: bacd Actual:" + reverseStr("abcd", 2));
-        System.out.println("Expected: bacdfeg Actual:" + reverseStr("abcdefg", 2));
-        System.out.println("Expected: dcba Actual:" + reverseStr("abcd", 4));
-        System.out.println("Expected: gfedcba Actual:" + reverseStr("abcdefg", 8));
+        System.out.println("Expected: bacd   Actual:" + reverseStr("abcd", 2));
+        System.out.println("Expected: bacd feg   Actual:" + reverseStr("abcdefg", 2));
+        System.out.println("Expected: dcba   Actual:" + reverseStr("abcd", 4));
+        System.out.println("Expected: gfed cba   Actual:" + reverseStr("abcdefg", 8));
     }
 }
