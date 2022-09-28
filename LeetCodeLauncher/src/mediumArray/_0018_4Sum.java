5b4ec2188f0235538fc016771b9dc38b9f8c90d8
mjoniec <mart1n.joniec@gmail.com>
2022-09-28 20:29:02 +0200

    _0018_4Sum - time limit exceeded

--- LeetCodeLauncher/src/mediumArray/_0018_4Sum.java
+++ LeetCodeLauncher/src/mediumArray/_0018_4Sum.java
@@ -16,49 +16,31 @@ public class _0018_4Sum {
         Collections.sort(list);
         _numsAsc = list.stream().mapToInt(Integer::intValue).toArray();
 
-        selectA();
-
-        return  _lists;
-    }
-
-    private void selectA(){
-        for (var a = 0; a<_numsAsc.length; a++) selectB(a);
-    }
-
-    private void selectB(int a){
-        for (var b = a+1; b<_numsAsc.length; b++) {
-            if(_numsAsc[a] + _numsAsc[b] <= _target) selectC(a, b);
-            else break;
-        }
-    }
-
-    private void selectC(int a, int b){
-        for (var c = b+1; c<_numsAsc.length; c++) {
-            if(_numsAsc[a] + _numsAsc[b] + _numsAsc[c] <= _target) selectD(a, b, c);
-            else break;
-        }
-    }
-
-    private void selectD(int a, int b, int c){
+        for (var a = 0;   a<_numsAsc.length; a++)
+        for (var b = a+1; b<_numsAsc.length; b++)
+        for (var c = b+1; c<_numsAsc.length; c++)
         for (var d = c+1; d<_numsAsc.length; d++) {
             if(_numsAsc[a] + _numsAsc[b] + _numsAsc[c] + _numsAsc[d] < _target) continue;
             if(_numsAsc[a] + _numsAsc[b] + _numsAsc[c] + _numsAsc[d] == _target) {
-                var list = new ArrayList<Integer>();
-                list.add(_numsAsc[a]);
-                list.add(_numsAsc[b]);
-                list.add(_numsAsc[c]);
-                list.add(_numsAsc[d]);
-                if(!_lists.stream().anyMatch(l -> l.equals(list))){
-                    _lists.add(list);
+                var l = new ArrayList<Integer>();
+                l.add(_numsAsc[a]);
+                l.add(_numsAsc[b]);
+                l.add(_numsAsc[c]);
+                l.add(_numsAsc[d]);
+                if(!_lists.stream().anyMatch(e -> e.equals(l))){
+                    _lists.add(l);
                 }
             }
             else break;
         }
+
+        return  _lists;
     }
 
     public void run() {
         System.out.println("_0018_4Sum");
         System.out.println("Expected: [-2,-1,1,2],[-2,0,0,2],[-1,0,0,1] Actual: " + fourSum(new int[]{1,0,-1,0,-2,2}, 0));
+        System.out.println("Expected: [-5,-4,-3,1] Actual: " + fourSum(new int[]{1,-2,-5,-4,-3,3,3,5}, -11));
         System.out.println("Expected: [2,2,2,2] Actual: " + fourSum(new int[]{2,2,2,2,2}, 8));
     }
 }
