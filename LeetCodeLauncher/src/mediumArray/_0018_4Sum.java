861b89fa2fdc053c53686fc2e1d4ec00af40b30c
mjoniec <mart1n.joniec@gmail.com>
2022-09-29 00:03:46 +0200

    _0018_4Sum - cast all to positive numbers - also does not pass

--- LeetCodeLauncher/src/mediumArray/_0018_4Sum.java
+++ LeetCodeLauncher/src/mediumArray/_0018_4Sum.java
@@ -5,7 +5,46 @@ import java.util.Arrays;
 import java.util.List;
 
 public class _0018_4Sum {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+    //sort ascending
+    //add min+1 to all - all elements are larger than 0 (int overflow)
+    //
+    public List<List<Integer>> fourSum(int[] numsI, int targetI) {
+        int n = numsI.length;
+        List<List<Integer>> _lists = new ArrayList<>();
+        Arrays.sort(numsI);
+        long minFactor = Math.abs(numsI[0])+1;
+        long[] nums = new long[n];
+        for(var i = 0; i<n; i++) nums[i] = numsI[i] + minFactor;
+        long target = targetI + 4 * minFactor;
+
+        for (var a = 0; a< n; a++) {
+            if (nums[a] > target) break;
+            for (var b = a+1; b< n; b++) {
+                if (nums[a] + nums[b] > target) break;
+                for (var c = b+1; c< n; c++) {
+                    if (nums[a] + nums[b] + nums[c] > target) break;
+                    for (var d = c + 1; d < n; d++) {
+                        long sum = nums[a] + nums[b] + nums[c] + nums[d];
+                        if (sum > target) break;
+                        if (sum < target) continue;
+                        if (sum == target) {
+                            var l = new ArrayList<Integer>();
+                            l.add((int) (nums[a] - minFactor));
+                            l.add((int) (nums[b] - minFactor));
+                            l.add((int) (nums[c] - minFactor));
+                            l.add((int) (nums[d] - minFactor));
+                            if (!_lists.stream().anyMatch(e -> e.equals(l))) {
+                                _lists.add(l);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return  _lists;
+    }
+
+    public List<List<Integer>> fourSumBruteForce(int[] nums, int target) {
         int n = nums.length;
         List<List<Integer>> _lists = new ArrayList<>();
         Arrays.sort(nums);
