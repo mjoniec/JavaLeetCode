d28c3deeb3ca91c4def16604ec8ee2d81f701377
mjoniec <mart1n.joniec@gmail.com>
2022-09-29 21:07:56 +0200

    _0016_3SumClosest

--- LeetCodeLauncher/src/mediumArray/_0016_3SumClosest.java
+++ LeetCodeLauncher/src/mediumArray/_0016_3SumClosest.java
@@ -4,33 +4,24 @@ import java.util.Arrays;
 
 public class _0016_3SumClosest {
     public int threeSumClosest(int[] nums, int target) {
-        int closestSolution = Integer.MAX_VALUE;
+        int minDiff = Integer.MAX_VALUE;
         Arrays.sort(nums);
-
-        for(int i = 0; i<nums.length - 2; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue; //duplicate val forward as covered in previous step
-            int j = i+1;//sliding pincer window
-            int k = nums.length-1;
-
-            while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) return target;//perfect solution found we can break
-                if (Math.abs(sum - target) < Math.abs(closestSolution - target)) closestSolution = sum;
-                if(sum > 0) { //move only one pincer
-                    k--;
-                    while (j < k && nums[k] == nums[k + 1]) k--; //duplicate val forward
-                } else {
-                    j++;
-                    while (j < k && nums[j] == nums[j - 1]) j++; //duplicate val forward
-                }
+        for(var i = 0; i<nums.length-2; i++){
+            var j = i+1;
+            var k = nums.length-1;
+            while(j<k){//sliding pincer window
+                var sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) return target; //perfect solution found we can break
+                if (Math.abs(target - sum) < Math.abs(minDiff)) minDiff = target - sum; //closer solution
+                if (sum > target) k--; else j++; //move only one pincer
             }
         }
-
-        return closestSolution;
+        return target - minDiff;
     }
 
     public void run() {
-        System.out.println("_0015_3Sum: ");
+        System.out.println("_0016_3SumClosest: ");
         System.out.println("Given: [-1,2,1,-4] target:1   Expected: 2 Actual: " + threeSumClosest(new int[]{-1,2,1,-4}, 1));
+        System.out.println("Given: [4,0,5,-5,3,3,0,-4,-5] target:-2   Expected:-2  Actual: " + threeSumClosest(new int[]{4,0,5,-5,3,3,0,-4,-5}, -2));
     }
 }
